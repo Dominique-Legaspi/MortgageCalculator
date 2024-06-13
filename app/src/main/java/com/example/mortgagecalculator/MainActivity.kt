bdb5f7dbfb481512af3efc1a914048fed20a044e
Dominique-Legaspi <domi.legaspi1006@gmail.com>
2024-06-12 22:16:45 -0700

    navigation starter

--- app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
+++ app/src/main/java/com/example/mortgagecalculator/MainActivity.kt
@@ -4,23 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.mortgagecalculator.ui.theme.MortgageApp
 import com.example.mortgagecalculator.ui.theme.MortgageCalculatorTheme
-import com.example.mortgagecalculator.ui.theme.MortgageScreen
-import com.example.mortgagecalculator.ui.theme.MortgageViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +19,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    MortgageScreen()
+                    MortgageApp()
                 }
             }
         }
--- app/src/main/java/com/example/mortgagecalculator/ui/theme/MortgageModifyScreen.kt
+++ app/src/main/java/com/example/mortgagecalculator/ui/theme/MortgageModifyScreen.kt
@@ -1,34 +1,53 @@
 package com.example.mortgagecalculator.ui.theme
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mortgagecalculator.R
 
 @Composable
 fun MortgageModifyScreen(
-    mortgageViewModel: MortgageViewModel = viewModel()
+    onDoneClicked: () -> Unit,
+    mortgageViewModel: MortgageViewModel = viewModel(),
+    modifier: Modifier = Modifier
 ) {
     val mortgageUiState by mortgageViewModel.uiState.collectAsState()
+    var newAmount by remember { mutableStateOf("") }
 
-    Column (
-        Modifier.padding(top = 20.dp)
+    Column (modifier = modifier,
+        verticalArrangement = Arrangement.SpaceBetween
     ){
         Row (modifier = Modifier.padding(start = 14.dp, 2.dp),
             horizontalArrangement = Arrangement.Center,
@@ -46,11 +65,18 @@ fun MortgageModifyScreen(
         RowInfo(
             name = "Amount",
             data = "$10000"
+
         )
         RowInfo(
             name = "Interest Rate",
             data = "3.5%"
         )
+        Button(onClick = onDoneClicked,
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.CenterHorizontally)) {
+                Text(text = "Done")
+        }
     }
 }
 
@@ -80,10 +106,4 @@ fun YearSelection() {
                 modifier = Modifier.padding(top = 10.dp))
         }
     }
-}
-
-@Preview
-@Composable
-fun ModifyPreview() {
-    MortgageModifyScreen()
 }
